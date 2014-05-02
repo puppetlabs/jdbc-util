4e228f830c653b1c22c0679bf12ff0a5880d5259
Nathaniel Smith <nathanielksmith@gmail.com>
2014-05-02 13:54:00 -0400

    WIP add some stuff

--- src/puppetlabs/jdbc-util/core.clj
+++ src/puppetlabs/jdbc-util/core.clj
@@ -1,1 +1,24 @@
-(ns puppetlabs.jdbc-util)
+(ns puppetlabs.jdbc-util
+  (:require [clojure.java.jdbc           :as jdbc]
+            [puppetlabs.kitchensink.core :as ks])
+(defn- convert-result-arrays
+  "Converts Java and JDBC arrays in a result set using the provided function
+   (eg. vec, set). Values which aren't arrays are unchanged."
+  ([result-set]
+     (convert-result-arrays vec result-set))
+  ([f result-set]
+     (let [convert #(cond
+                     (ks/array? %) (f %)
+                     (isa? (class %) java.sql.Array) (f (.getArray %))
+                     :else %)]
+       (map #(ks/mapvals convert %) result-set))))
+
+(defn- query
+  "An implementation of query that returns a fully evaluated result (no
+   JDBCArray objects, etc)"
+  [db sql-and-params]
+  (let [convert (fn [rs]
+                  (doall
+                   (convert-result-arrays vec
+                                          (jdbc/result-set-seq rs))))]
+    (jdbc/db-query-with-resultset db sql-and-params convert)))
