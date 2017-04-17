a8d5b04427b0139c56da78b8e20d942bbb77b81d
aperiodic <dlp@aperiodic.org>
2017-04-17 16:05:42 -0700

    Version 0.6.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject puppetlabs/jdbc-util "0.6.2-SNAPSHOT"
+(defproject puppetlabs/jdbc-util "0.6.2"
   :description "Common JDBC helpers for use in Puppet Labs projects"
   :url "https://github.com/puppetlabs/jdbc-util"
 
