7eb1795a5ac78b6cc80b7a42ad6265ede6810bda
Jenkins CI <ci@puppet.com>
2018-07-13 21:26:38 +0000

    Version 1.2.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject puppetlabs/jdbc-util "1.2.0"
+(defproject puppetlabs/jdbc-util "1.2.1-SNAPSHOT"
   :description "Common JDBC helpers for use in Puppet Labs projects"
   :url "https://github.com/puppetlabs/jdbc-util"
 
